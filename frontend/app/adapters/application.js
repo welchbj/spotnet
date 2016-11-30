5247492bdb90f712c2ed9e8d5a2807ada6a9df4e
welchbj <welch18@vt.edu>
2016-11-30 09:50:44 -0500

    Add application adapter to interface with Spotify Web API

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,30 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+import ENV from '../config/environment';
+
+export default DS.JSONAPIAdapter.extend({
+
+  spotify: Ember.inject.service(),
+
+  host: ENV.SPOTIFY_WEB_API_HOST,
+
+  namespace: ENV.SPOTIFY_WEB_API_NAMESPACE,
+
+  headers: Ember.computed('spotify.accessToken', {
+    get() {
+      let headers = {};
+      const accessToken = this.get('spotify.accessToken');
+
+      if (accessToken) {
+        headers['Authorization'] = 'Bearer ' + accessToken;
+      }
+      else {
+        console.log('No access token for Spotify; something will probably ' +
+                    'go wrong soon.');
+      }
+
+      return headers;
+    }
+  })
+
+});
